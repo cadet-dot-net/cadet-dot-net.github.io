dad547f9ac3d2b5da083d05fa97de33973a55195
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-03-12 02:06:17 +0000

    add basic html portfolio view

--- lib/github_pages_web/router.ex
+++ lib/github_pages_web/router.ex
@@ -18,6 +18,7 @@ defmodule GithubPagesWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/portfolio", PortfolioController, :show
   end
 
   # Other scopes may use custom stacks.
